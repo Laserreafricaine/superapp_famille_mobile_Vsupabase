3087920a709f333325504d113a688192454b5040
Laserreafricaine <monpotagermaison@gmail.com>
2026-06-01 09:37:59 +0200

    pwa

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'superapp-famille-v5-36-19';
-const CORE_ASSETS = ['./', './index.html', './css/app.css?v=5.36.19', './js/app.js?v=5.36.19', './js/supabase-client.js?v=5.36.19', './js/supabase-app.js?v=5.36.19', './css/supabase-auth.css?v=5.36.19', './manifest.json?v=5.36.19'];
+const CACHE = 'superapp-famille-v5-36-21';
+const CORE_ASSETS = ['./', './index.html', './css/app.css?v=5.36.20', './js/app.js?v=5.36.20', './js/supabase-client.js?v=5.36.20', './js/supabase-app.js?v=5.36.20', './css/supabase-auth.css?v=5.36.20', './manifest.json?v=5.36.21'];
 self.addEventListener('install', event => {
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE_ASSETS)).catch(()=>{}));
